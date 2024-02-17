61f768244c75ddc6295d85a1c5c4c0059cee6246
Ziyin Sun <80473654+Drakse@users.noreply.github.com>
2024-02-17 15:53:37 +0100

    Add files via upload

---
+++ parabolic.py
@@ -0,0 +1,22 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+
+print("Enter initial velocity in m/s")
+v = float(input())
+g = 9.8 #Gravity acceleration in m/s2
+theta = np.arange(0,90,10) #List of the angle
+theta = np.radians(theta) #Convert degrees to radians
+t = np.arange(0,10,0.1) #Define the time interval setting as a continous parameter
+#Calculate trajectory for every angle
+for u in theta: #Trajectory for each angle
+    x = [] #Empty list of x and y coordinates
+    y = []
+    for k in t: #Create a loop for calculating the coordinates
+        x.append(v*k*np.cos(u))
+        y.append(v*k*np.sin(u)-0.5*g*k*k)
+    #Plot the results
+    plt.plot(x, y)
+    plt.xlabel('Distance (m)')
+    plt.ylabel('Height (m)')
+    plt.title('Projectile motion')
+plt.show()
